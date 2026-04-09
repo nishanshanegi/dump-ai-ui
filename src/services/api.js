3a8d793026b25a7218c7bd20146bafde5df521cf
Nishansha Negi <nishanegi16112001@gmail.com>
2026-04-09 15:32:54 +0530

    render

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://13.60.250.81:8000/api/v1";
+const API_BASE_URL = "https://dumpai-api.onrender.com/api/v1";
 const api = axios.create({
     baseURL: API_BASE_URL,
 });
-
+//
 // WHAT: Interceptor
 // WHY: This automatically adds your JWT token to EVERY request so you don't have to.
 api.interceptors.request.use((config) => {
